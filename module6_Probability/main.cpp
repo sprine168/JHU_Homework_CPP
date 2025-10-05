3461eecd70f2ca73a5a4fb4ef3827d195cd82c60
Steven P. <sprine20@gmail.com>
2025-10-04 23:17:43 -0400

    Update main.cpp
    Added an event generator, to easily generate events of checking probabilities.

--- module6_Probability/main.cpp
+++ module6_Probability/main.cpp
@@ -4,10 +4,10 @@
 
 class Probability {
 private:
-    double value;
+    double value = 0.0;
 
     // Probabilities must be between 0 and 1
-    void validateInput(double &p) const {
+    static void validateInput(double &p) {
         if (p < 0 || p > 1) {
             throw std::out_of_range("Invalid probability. Probability must be between 0.0 and 1.0");
         }
@@ -21,12 +21,12 @@ public:
         validateInput(p);
         this->value = p;
     }
+    Probability() { }
 
-    /**
-     *  ---------------------------------------------------
-     *      Operator Overloading for Independent Events
-     *  ---------------------------------------------------
-     */
+
+    /*---------------------------------------------------
+         Operator Overloading for Independent Events
+     ---------------------------------------------------*/
 
     // Logical AND: A & B => ( A * B )
     Probability operator&(const Probability &other) const { return {this->value * other.value}; }
@@ -47,15 +47,11 @@ public:
     // NOT: ~A => ( 1 - A )
     Probability operator~() const { return {1.0 - this->value}; }
 
-
-    /**
-     *  ---------------------------------------------
-     *         Getters and Setters Start Here
-     *  ---------------------------------------------
-     */
-
+     /*---------------------------------------------
+                Getters and Setters Start Here
+      ---------------------------------------------*/
     // getter for the probability entered
-    double getProbability() {
+    double getValue() const {
         return this->value;
     }
 
@@ -65,7 +61,7 @@ class Event {
     double eventNumber;
 
 private:
-    static double randomNumber() {
+    void randomNumber() {
         // Getting clock time from the local computer for the seed
         std::random_device randomDevice;
 
@@ -75,66 +71,80 @@ private:
         // Setting the range of distribution
         std::uniform_real_distribution<> distribution(0, 1);
 
-        // Generating the random number
-        return distribution(mtGenerator);
+        // Generating the random number and storing it in the eventNumber data member
+        this->eventNumber = distribution(mtGenerator);
     }
 
 public:
-    Event() { this->eventNumber = randomNumber(); }
+    Event() { randomNumber(); }
 
+    void generateProbability() { randomNumber(); }
     double getEventNumber() const { return this->eventNumber; }
+};
+
+
+// Generate events based on a random number and the probabilities or A and/or B
+class EventGenerator {
+private:
+    // Initializing a and b with a default constructor
+    Probability a {};
+    Probability b {};
+
+    // Initializing the event a and event b
+    Event eventA {};
+    Event eventB {};
+
+public:
+    EventGenerator(const Probability &a, const Probability &b, const Event &eventA, const Event &eventB) {
+        this->eventA = eventA;
+        this->eventB = eventB;
+        this->a = a;
+        this->b = b;
+    }
+
+    void generateEvent(const int &eventNumber) const {
+        std::cout << "---------    Generated Event: " << eventNumber << " ------------" << std::endl;
+        std::cout << "A: " << a.getValue() << std::endl;
+        std::cout << "B: " << b.getValue() << std::endl;
+
+        std::cout << "  NOT A:  "        << (~a).getValue()         << std::endl;
+        std::cout << "  NOT B:  "        << (~b).getValue()         << std::endl;
+        std::cout << "A  &  B:  "        << (a & b).getValue()      << std::endl;
+        std::cout << "A  OR B:  "        << (a | b).getValue()      << std::endl;
+        std::cout << "A XOR B:  "        << (a ^ b).getValue()      << std::endl;
+        std::cout << "NOT A or NOT B: "  << 1 - (a | b).getValue()  << std::endl;
+        std::cout << "A NOT B:  "        << (a - b).getValue()      << std::endl;
+        std::cout << "B NOT A:  "        << (b - a).getValue()      << std::endl;
+        std::cout << "----------------------------------------------------" << std::endl;
+    }
 };
 
 
 int main() {
 
     try {
+        // Running a hardcoded test
+        Event eventA;
+        Event eventB;
 
-        // Generic test one
         Probability a(0.04);
         Probability b(0.02);
 
-        std::cout << "A: " << a.getProbability() << std::endl;
-        std::cout << "B: " << b.getProbability() << std::endl;
+        EventGenerator event(a, b, eventA, eventB);
+        event.generateEvent(0);
 
-        std::cout << "  NOT A:  "        << (~a).getProbability()         << std::endl;
-        std::cout << "  NOT B:  "        << (~b).getProbability()         << std::endl;
-        std::cout << "A  &  B:  "        << (a & b).getProbability()      << std::endl;
-        std::cout << "A  OR B:  "        << (a | b).getProbability()      << std::endl;
-        std::cout << "A XOR B:  "        << (a ^ b).getProbability()      << std::endl;
-        std::cout << "NOT A or NOT B: "  << 1 - (a | b).getProbability()  << std::endl;
-        std::cout << "A NOT B:  "        << (a - b).getProbability()      << std::endl;
-        std::cout << "B NOT A:  "        << (b - a).getProbability()      << std::endl;
-        std::cout << "Ended hardcoded test \n" << std::endl;
-        std::cout << "----------------------------------------------------\n";
-
-
-        // Checked test results here:
-        // https://www.calculator.net/probability-calculator.html?cal1pa=0.04&cal1pb=0.02&calctype=twoe&x=Calculate
-
-        // Simulating 10 random probability events
-        for (int i = 1; i <= 10; i++) {
-            std::cout << "Test Event: " << i << std::endl;
-            const Event eventA;
-            const Event eventB;
+        // Running 10 random events through an event generator
+        for (int i = 1; i <= 4; i++) {
+            Event eventA;
+            Event eventB;
 
             Probability a(eventA.getEventNumber());
             Probability b(eventB.getEventNumber());
 
-            std::cout << "A: " << a.getProbability() << std::endl;
-            std::cout << "B: " << b.getProbability() << std::endl;
-
-            std::cout << "  NOT A:  "        << (~a).getProbability()         << std::endl;
-            std::cout << "  NOT B:  "        << (~b).getProbability()         << std::endl;
-            std::cout << "A  &  B:  "        << (a & b).getProbability()      << std::endl;
-            std::cout << "A  OR B:  "        << (a | b).getProbability()      << std::endl;
-            std::cout << "A XOR B:  "        << (a ^ b).getProbability()      << std::endl;
-            std::cout << "NOT A or NOT B: "  << 1 - (a | b).getProbability()  << std::endl;
-            std::cout << "A NOT B:  "        << (a - b).getProbability()      << std::endl;
-            std::cout << "B NOT A:  "        << (b - a).getProbability()      << std::endl;
-
-            std::cout << "----------------------------------------------------" << std::endl;
+            EventGenerator event = EventGenerator(a, b, eventA, eventB);
+            event.generateEvent(i);
         }
+
 
     } catch (const std::out_of_range &e) {
         std::cout << e.what() << std::endl;
