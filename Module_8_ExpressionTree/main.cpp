b000b003370d5add5beed40d55858e7e9e6d3376
Steven P. <sprine20@gmail.com>
2025-10-25 18:31:35 -0400

    Update main.cpp
    Tested calculations and code.

--- Module_8_ExpressionTree/main.cpp
+++ Module_8_ExpressionTree/main.cpp
@@ -4,7 +4,6 @@
 #include <memory>
 
 /**
- *
  *      ===================================================================================================
  *              Cpp 11 is being used for this assignment, so the static declaration has
  *                  internal and external to the class, for memory allocation purposes.
@@ -15,7 +14,10 @@
  * Step 1:
  * Set up the classes necessary for the requirements: Node, Constant, Variable, Add, Sub, Mul, Div.
  *
- *      Requirement creep happened, and I had to add a parent class called Operator that contains Add, Sub, Mul, Div,
+ *      I used an operator parent class, using a composite pattern for this assignment which is good for tree structures.
+ *          The operator parent class that adds "leaf" nodes for left and right for the tree structure.
+ *          The sub classes of Operator have the ability to calculate derivatives and expressions
+ *          Add, Sub, Mul, Div
  *          so that I could make "leaf" nodes for left and right for the tree structure.
  *
  * Step 2:  Set up the print function in each class to print out where it is in the Node structure
@@ -29,10 +31,11 @@
 /**
  *  Node is the base class for the expression tree in this assignment
  *
- *  Subclasses will include: Add, Sub, Mul, Div, Constant, Variable
- *  These subclasses will have left and right tree leaf nodes
- *  The subclasses will be able to print their part of the tree
- *  The subclasses shall complete derivatives for their type of expression (add rule, sub rule, quotient rule, etc.)
+ *  Subclasses for the Node Class shall include: Constant, Variable
+ *  Operator subclass of Node shall include: Add, Sub, Mul, Div,
+ *      These subclasses will have left and right tree leaf nodes
+ *      The subclasses will be able to print their part of the tree
+ *      The subclasses shall complete derivatives for their type of expression (add rule, sub rule, quotient rule, etc.)
  *
  *  The node shall have a toString or similar function that can print out the entire expression tree.
  *
@@ -59,14 +62,16 @@ public:
     virtual std::shared_ptr<Node> derivative(const std::string &var) const = 0;
 
     // Note: Using C++ 11 so I can't use an inline declaration and initialization
-    static std::shared_ptr<std::map<std::string, double>> symbolTable;
+    static std::shared_ptr<std::map<std::string, double> > symbolTable;
 };
+
 
 class Constant : public Node {
     double value;
 
 public:
-    explicit Constant(const double &v) : value(v) { }
+    explicit Constant(const double &v) : value(v) {
+    }
 
     double evaluate() const override {
         return this->value;
@@ -76,6 +81,7 @@ public:
     std::shared_ptr<Node> derivative(const std::string &var) const override {
         return std::make_shared<Constant>(0);
     }
+
     std::string toString() const override { return "(" + std::to_string(this->value) + ")"; }
 };
 
@@ -84,7 +90,8 @@ class Variable : public Node {
     std::string name;
 
 public:
-    explicit Variable(const std::string &name) : name(name) {}
+    explicit Variable(const std::string &name) : name(name) {
+    }
 
     double evaluate() const override {
         if (Node::symbolTable == nullptr || Node::symbolTable->empty()) { return 0; }
@@ -96,7 +103,7 @@ public:
     }
 
     // If there is a variable, it will either be a 1 or a 0
-    std::shared_ptr<Node> derivative(const std::string& var) const override {
+    std::shared_ptr<Node> derivative(const std::string &var) const override {
         if (name == var) { return std::make_shared<Constant>(1.0); }
         return std::make_shared<Constant>(0.0);
     }
@@ -112,53 +119,63 @@ protected:
     std::shared_ptr<Node> right;
 
 public:
-    Operator(const std::shared_ptr<Node> &left, const std::shared_ptr<Node> &right) : left(left), right(right) { }
+    Operator(const std::shared_ptr<Node> &left, const std::shared_ptr<Node> &right) : left(left), right(right) {
+    }
 };
 
 
 class Add : public Operator {
 public:
-    Add(const std::shared_ptr<Node> &left, const std::shared_ptr<Node> &right) : Operator(left, right) { }
+    Add(const std::shared_ptr<Node> &left, const std::shared_ptr<Node> &right) : Operator(left, right) {
+    }
 
     double evaluate() const override {
         return this->left->evaluate() + this->right->evaluate();
     }
 
-    std::shared_ptr<Node> derivative(const std::string& var) const override {
+    std::shared_ptr<Node> derivative(const std::string &var) const override {
         std::shared_ptr<Node> du = left->derivative(var);
         std::shared_ptr<Node> dv = right->derivative(var);
         return std::make_shared<Add>(du, dv);
     }
-    std::string toString() const override { return "(" + this->left->toString() + " + " + this->right->toString() + ")"; }
+
+    std::string toString() const override {
+        return "(" + this->left->toString() + " + " + this->right->toString() + ")";
+    }
 };
 
 
 class Sub : public Operator {
 public:
-    Sub(const std::shared_ptr<Node> &left, const std::shared_ptr<Node> &right) : Operator(left, right) { }
+    Sub(const std::shared_ptr<Node> &left, const std::shared_ptr<Node> &right) : Operator(left, right) {
+    }
 
     double evaluate() const override {
         return this->left->evaluate() - this->right->evaluate();
     }
 
-    std::shared_ptr<Node> derivative(const std::string& var) const override {
+    std::shared_ptr<Node> derivative(const std::string &var) const override {
         std::shared_ptr<Node> du = left->derivative(var);
         std::shared_ptr<Node> dv = right->derivative(var);
         return std::make_shared<Sub>(du, dv);
     }
-    std::string toString() const override { return "(" + this->left->toString() + " - " + this->right->toString() + ")"; }
+
+    std::string toString() const override {
+        return "(" + this->left->toString() + " - " + this->right->toString() + ")";
+    }
 };
 
 
 class Mul : public Operator {
 public:
-    Mul(const std::shared_ptr<Node> &left, const std::shared_ptr<Node> &right) : Operator(left, right) { }
+    Mul(const std::shared_ptr<Node> &left, const std::shared_ptr<Node> &right) : Operator(left, right) {
+    }
 
     double evaluate() const override {
         return this->left->evaluate() * this->right->evaluate();
     }
 
-    std::shared_ptr<Node> derivative(const std::string& var) const override {
+    std::shared_ptr<Node> derivative(const std::string &var) const override {
         std::shared_ptr<Node> du = left->derivative(var);
         std::shared_ptr<Node> dv = right->derivative(var);
 
@@ -167,20 +184,24 @@ public:
 
         return std::make_shared<Add>(leftDu, rightDv);
     }
-    std::string toString() const override { return "(" + this->left->toString() + " * " + this->right->toString() + ")"; }
+
+    std::string toString() const override {
+        return "(" + this->left->toString() + " * " + this->right->toString() + ")";
+    }
 };
 
 
 class Div : public Operator {
 public:
-    Div(const std::shared_ptr<Node> &left, const std::shared_ptr<Node> &right) : Operator(left, right) { }
+    Div(const std::shared_ptr<Node> &left, const std::shared_ptr<Node> &right) : Operator(left, right) {
+    }
 
     double evaluate() const override {
         return this->left->evaluate() / this->right->evaluate();
     }
 
 
-    std::shared_ptr<Node> derivative(const std::string& var) const override {
+    std::shared_ptr<Node> derivative(const std::string &var) const override {
         std::shared_ptr<Node> du = left->derivative(var);
         std::shared_ptr<Node> dv = right->derivative(var);
         std::shared_ptr<Node> numerator = std::make_shared<Sub>(
@@ -190,13 +211,17 @@ public:
         std::shared_ptr<Node> denominator = std::make_shared<Mul>(right, right);
         return std::make_shared<Div>(numerator, denominator);
     }
-    std::string toString() const override { return "(" + this->left->toString() + " / " + this->right->toString() + ")"; }
+
+    std::string toString() const override {
+        return "(" + this->left->toString() + " / " + this->right->toString() + ")";
+    }
 };
 
-std::shared_ptr<std::map<std::string, double>> Node::symbolTable = std::make_shared<std::map<std::string, double>>();
+
+// Static variable with a shared pointer has to be declared outside the class for defining and initializing in c++ 11
+std::shared_ptr<std::map<std::string, double> > Node::symbolTable = std::make_shared<std::map<std::string, double> >();
 
 int main() {
-
     Node::symbolTable->emplace("Xray", 2.0);
     Node::symbolTable->emplace("Yellow", 3.0);
     Node::symbolTable->emplace("Zebra", 5.0);
@@ -216,38 +241,99 @@ int main() {
         )
     );
 
-    std::shared_ptr<Node> node1 = originalNode;
-    std::shared_ptr<Node> node2 = originalNode;
-    std::shared_ptr<Node> node3 = originalNode;
+    /*
+     *  =========================================================
+     *                        Testing Section
+     *  =========================================================
+     */
+    std::shared_ptr<Node> test1 = originalNode;
+    std::shared_ptr<Node> test2 = originalNode;
+    std::shared_ptr<Node> test3 = originalNode;
+    std::shared_ptr<Node> test4 = originalNode;
 
     /*     Expression Test One     */
-    std::cout << node1->toString() << std::endl;                    // Print out the expression tree
-    double resultOne = node1->evaluate();                           // Evaluate the expression
-    std::cout << "Result d = " << resultOne << "\n" << std::endl;
+    std::cout << test1->toString() << std::endl;
+    double resultOne = test1->evaluate();
+    std::cout << "Expression Result = " << resultOne << "\n" << std::endl;
 
     /*     Derivative Test One     */
-    node1 = node1->derivative("Xray");
-    std::cout << "Xray Derivative: " << node1->toString() << std::endl;
-    double derivativeResult = node1->evaluate();
-    std::cout << "Result = " << derivativeResult << std::endl;
+    test1 = test1->derivative("Xray");
+    std::cout << "Xray Derivative: " << test1->toString() << std::endl;
+    double derivativeResult = test1->evaluate();
+    std::cout << "Derivative Result = " << derivativeResult << std::endl;
 
     std::cout << std::endl;
 
     /*      Derivative Test Two    */
-    node2 = node2->derivative("Yellow");
-    std::cout << "Yellow Derivative: " << node2->toString() << std::endl;
-    derivativeResult = node2->evaluate();
-    std::cout << "Result = " << derivativeResult << std::endl;
+    test2 = test2->derivative("Yellow");
+    std::cout << "Yellow Derivative: " << test2->toString() << std::endl;
+    derivativeResult = test2->evaluate();
+    std::cout << "Derivative Result = " << derivativeResult << std::endl;
 
     std::cout << std::endl;
 
     /*      Derivative Test Three    */
-    node3 = node3->derivative("Zebra");
-    std::cout << "Zebra Derivative: " << node3->toString() << std::endl;
-    derivativeResult = node3->evaluate();
-    std::cout << "Result: = " << derivativeResult << std::endl;
+    test3 = test3->derivative("Zebra");
+    std::cout << "Zebra Derivative: " << test3->toString() << std::endl;
+    derivativeResult = test3->evaluate();
+    std::cout << "Derivative Result: = " << derivativeResult << std::endl;
 
     std::cout << std::endl;
 
+    /*      Derivative Test Four    */
+    test4 = test4->derivative("Xray");
+    std::cout << "Xray Derivative: " << test4->toString() << std::endl;
+    derivativeResult = test4->evaluate();
+    std::cout << "Derivative Result: = " << derivativeResult << std::endl;
+
+    std::cout << std::endl;
+
+    /*
+     *  =================================================
+     *                  Expression Testing:
+     * ==================================================
+     */
+    // Hand construct: 2.3 * Xray + Yellow * (Zebra - Xray)
+    const std::shared_ptr<Node> testNode1 = std::make_shared<Add>(
+        std::make_shared<Sub>(
+            std::make_shared<Constant>(50),
+            std::make_shared<Mul>(
+                std::make_shared<Variable>("Xray"),
+                std::make_shared<Constant>(2)
+            )
+        ),
+        std::make_shared<Add>(
+            std::make_shared<Variable>("Yellow"),
+            std::make_shared<Sub>(
+                std::make_shared<Variable>("Zebra"),
+                std::make_shared<Variable>("Xray")
+            )
+        )
+    );
+
+    /*     Expression Test One     */
+    std::cout << testNode1->toString() << std::endl;
+    double testNode1Result = testNode1->evaluate();
+    std::cout << "Expression Result = " << testNode1Result << std::endl;
+
+    std::cout << std::endl;
+
+    derivativeResult = testNode1->derivative("Xray")->evaluate();
+    std::cout << "Xray Derivative = " << testNode1->toString() << std::endl;
+    std::cout << "Derivative Result = " << derivativeResult << std::endl;
+
+    std::cout << std::endl;
+
+    derivativeResult = testNode1->derivative("Yellow")->evaluate();
+    std::cout << "yellow Derivative = " << testNode1->toString() << std::endl;
+    std::cout << "Derivative Result = " << derivativeResult << std::endl;
+
+    std::cout << std::endl;
+
+    derivativeResult = testNode1->derivative("Zebra")->evaluate();
+    std::cout << "Zebra Derivative = " << testNode1->toString() << std::endl;
+    std::cout << "Derivative Result = " << derivativeResult << std::endl;
+
+    std::cout << std::endl;
     return 0;
 }
